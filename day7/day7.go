3c5e03f53f70fade73f722bf080bcb85e0034078
Michiel Appelman <michiel@appelman.se>
2017-12-08 14:11:56 +0700

    Solves part 2 of day 7

--- day7/day7.go
+++ day7/day7.go
@@ -28,6 +28,72 @@ func StarOne(input []string) string {
 		}
 		programs[name] = &Program{name, weight, subprogs}
 	}
+	return findFirst(programs)
+}
+
+func StarTwo(input []string) int {
+	programs := make(map[string]*Program)
+	re := regexp.MustCompile(`(\w+) \((\d+)\)( -> (.+))?`)
+	for _, line := range input {
+		result := re.FindStringSubmatch(line)
+		name := result[1]
+		weight, _ := strconv.Atoi(result[2])
+		var subprogs []string
+		if len(result) > 3 {
+			subprogs = strings.Split(result[4], ", ")
+		}
+		programs[name] = &Program{name, weight, subprogs}
+	}
+	first := findFirst(programs)
+	_, newWeight := sumOfSubProgs(programs[first], programs, 0)
+	return newWeight
+}
+
+func sumOfSubProgs(program *Program, programs map[string]*Program, firstW int) (map[string]int, int) {
+	if len(program.subPrograms) <= 1 || len(program.subPrograms[0]) == 0 {
+		return map[string]int{program.name: program.weight}, firstW
+	}
+	weighted := make(map[string]int)
+	var newWeight int
+	for _, subprog := range program.subPrograms {
+		var w map[string]int
+		var nw int
+		w, nw = sumOfSubProgs(programs[subprog], programs, firstW)
+		if nw > 0 {
+			return nil, nw
+		}
+		for k, v := range w {
+			weighted[k] = v
+		}
+	}
+	uniqueWeights := make(map[int]int)
+	var weight int
+	for _, w := range weighted {
+		uniqueWeights[w]++
+		weight += w
+	}
+	var weirdWeight, correctWeight int
+	if len(uniqueWeights) > 1 && newWeight == 0 {
+		for k, v := range uniqueWeights {
+			if v == 1 {
+				weirdWeight = k
+			} else {
+				correctWeight = k
+			}
+		}
+		var weirdOwner string
+		for k, v := range weighted {
+			if v == weirdWeight {
+				weirdOwner = k
+			}
+		}
+		diff := weirdWeight - correctWeight
+		firstW = programs[weirdOwner].weight - diff
+	}
+	return map[string]int{program.name: weight + program.weight}, firstW
+}
+
+func findFirst(programs map[string]*Program) string {
 	hasPointer := make(map[string]bool, len(programs))
 	for _, prog := range programs {
 		hasPointer[prog.name] = false
@@ -43,10 +109,6 @@ func StarOne(input []string) string {
 	return ""
 }
 
-//func StarTwo(input string) int {
-//    ...
-//}
-
 func main() {
 	var instructions []string
 	scanner := bufio.NewScanner(os.Stdin)
@@ -54,5 +116,5 @@ func main() {
 		instructions = append(instructions, scanner.Text())
 	}
 	fmt.Println("1:", StarOne(instructions))
-	//fmt.Println("2:", StarTwo(instructions))
+	fmt.Println("2:", StarTwo(instructions))
 }
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -33,21 +33,31 @@ func TestStarOne(t *testing.T) {
 	}
 }
 
-//func TestStarTwo(t *testing.T) {
-//	tests := []struct {
-//		input string
-//		want  int
-//	}{
-//		{"1212", 6},
-//		{"1221", 0},
-//		{"123425", 4},
-//		{"123123", 12},
-//		{"12131415", 4},
-//	}
-//	for _, test := range tests {
-//		got := StarTwo(test.input)
-//		if got != test.want {
-//			t.Errorf("for %s got %d, want %d", test.input, got, test.want)
-//		}
-//	}
-//}
+func TestStarTwo(t *testing.T) {
+	tests := []struct {
+		input []string
+		want  int
+	}{
+		{[]string{
+			"pbga (66)",
+			"xhth (57)",
+			"ebii (61)",
+			"havc (66)",
+			"ktlj (57)",
+			"fwft (72) -> ktlj, cntj, xhth",
+			"qoyq (66)",
+			"padx (45) -> pbga, havc, qoyq",
+			"tknk (41) -> ugml, padx, fwft",
+			"jptl (61)",
+			"ugml (68) -> gyxo, ebii, jptl",
+			"gyxo (61)",
+			"cntj (57)",
+		}, 60},
+	}
+	for _, test := range tests {
+		got := StarTwo(test.input)
+		if got != test.want {
+			t.Errorf("for %v got %d, want %d", test.input, got, test.want)
+		}
+	}
+}
