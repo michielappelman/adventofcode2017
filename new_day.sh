702a0a688b143b0445373082b59094bae5fb4c8c
Michiel Appelman <michiel@appelman.se>
2017-12-15 18:04:53 +0100

    Changes templating of Go files

--- new_day.sh
+++ new_day.sh
@@ -16,24 +16,16 @@ cd $DAYNAME
 cat <<EOF > $DAYNAME.go
 package main
 
-import (
-	"github.com/michielappelman/adventofcode2017/generic"
-)
-
 func StarOne(input string) int {
-    ...
+    return 0
 }
-
-//func StarTwo(input string) int {
-//    ...
-//}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		input := scanner.Text()
 		fmt.Println("1:", StarOne(input))
-		fmt.Println("2:", StarTwo(input))
+		//fmt.Println("2:", StarTwo(input))
 	}
 }
 EOF
@@ -65,22 +57,4 @@ func TestStarOne(t *testing.T) {
 	}
 }
 
-//func TestStarTwo(t *testing.T) {
-//	tests := []struct {
-//		input string
-//		want  int
-//	}{
-//		{"1212", 6},
-//		{"1221", 0},
-//		{"123425", 4},
-//		{"123123", 12},
-//		{"12131415", 4},
-//	}
-//	for _, test := range tests {
-//		got := StarTwo(test.input)
-//		if got != test.want {
-//			t.Errorf("for %s got %d, want %d", test.input, got, test.want)
-//		}
-//	}
-//}
 EOF
